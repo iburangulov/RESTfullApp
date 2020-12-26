d63ec9763443a9b7e6c62be19444fcbfe758fd15
iburangulov <iburanguloff@ya.ru>
2020-12-26 01:26:34 -0600

    Api added for all get and post routes

--- controllers/PostsApiController.php
+++ controllers/PostsApiController.php
@@ -2,23 +2,40 @@
 
 namespace controllers;
 
-
-use components\DB;
+use models\PostsApiModel;
 
 class PostsApiController
 {
+    private $Posts;
+
+    public function __construct()
+    {
+        $this->Posts = new PostsApiModel('posts');
+    }
+
     public function getPost(int $id = 1)
     {
-        echo 'Post 1';
+        $post = $this->Posts->getById($id);
+        header('Content-type: application/json');
+        echo json_encode($post);
     }
 
     public function getPosts(int $page)
     {
-        echo 'From ' . (($page * 10) - 9) . ' to ' . ($page * 10);
+        $data = array();
+        for ($i = ($page * 10) - 9; $i < ($page * 10) + 1; $i++)
+        {
+            $result =  $this->Posts->getById($i);
+            if ($result) $data[] = $result;
+            else continue;
+        }
+        header('Content-type: application/json');
+        echo json_encode($data);
     }
 
     public function addPost()
     {
+
     }
 
     public function updatePost(int $id)
---
+++ models/PostsApiModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace models;
+
+use components\DB;
+use PDO;
+
+class PostsApiModel extends BaseModel
+{
+
+    public function getById($id)
+    {
+        $pdo = DB::getPDO();
+        if ($pdo) {
+            $result = DB::getByName($this->table, 'id', $id);
+            if ($result) return $result;
+            else return false;
+        } else return false;
+    }
+
+}
